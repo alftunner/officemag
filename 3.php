6aada1101dbfeac0f3fbf7cef3f36f13054fa874
Alexis <alftunner@gmail.com>
2020-08-20 14:10:28 +0300

    importXml with comment

--- 3.php
+++ 3.php
@@ -1,5 +1,5 @@
 <?php
-function dataBaseConnect()
+function dataBaseConnect() //функция для соединения с БД
 {
     $link = mysqli_connect("127.0.0.1", "alftunner", "", "test_samson");
 
@@ -15,7 +15,7 @@ function dataBaseConnect()
     return $link;
 }
 
-function getProductId($conn, $code, $name)
+function getProductId($conn, $code, $name) //функция для получения id текущего товара
 {
     $sql_select = "SELECT id from a_product WHERE code = '{$code}' and name = '{$name}' ORDER BY id DESC";
     $res = mysqli_query($conn, $sql_select);
@@ -23,7 +23,7 @@ function getProductId($conn, $code, $name)
     return $row['id'];
 }
 
-function insertToDB($conn, $sql)
+function insertToDB($conn, $sql) //функция для получения информации о успешном или неуспешном добавлении данных в БД
 {
     if (mysqli_query($conn, $sql)) {
         echo "\n" . "New record created successfully";
@@ -32,22 +32,19 @@ function insertToDB($conn, $sql)
     }
 }
 
-function insertPrice($conn, $product, $product_id)
+function insertPrice($conn, $product, $product_id) //функция для добаления цены в БД
 {
     foreach ($product->Цена as $price)
     {
         $price_type = $price["Тип"];
         $price_value = (double)$price["Значение"];
 
-        echo $price_type . "\n";
-        echo $price_value . "\n";
-
         $sql = "INSERT INTO a_price (product_id, type, price) VALUES ('{$product_id}', '{$price_type}', '{$price_value}')";
         insertToDB($conn, $sql);
     }
 }
 
-function insertProperty($conn, $product, $product_id)
+function insertProperty($conn, $product, $product_id) //функция для добавления свойств товара в БД
 {
     foreach ($product->Свойства as $property)
     {
@@ -56,23 +53,18 @@ function insertProperty($conn, $product, $product_id)
             $property_type = $key;
             $property_value = $value;
 
-            echo "\n" . "Свойства key = " . $key . "\n";
-            echo "\n" . "Свойства value = " . $value . "\n";
-
             $sql = "INSERT INTO a_property (product_id, type, value) VALUES ('{$product_id}', '{$property_type}', '{$property_value}')";
             insertToDB($conn, $sql);
         }
     }
 }
 
-function insertCategory($conn, $product, $product_id)
+function insertCategory($conn, $product, $product_id) //функция для добавления данных о категориях (в xml файле "Разделы"), если такой раздел существует, то записывает данные о связи продукта с категорией, если не существует, сначала создает категорию, затем устанавливает связь
 {
     foreach ($product->Разделы as $category)
     {
         foreach ($category as $key=>$value)
         {
-            echo "\n" . "category value = " . $value . "\n";
-
             $sql_select = "SELECT id from a_category WHERE name = '{$value}'";
             $res = mysqli_query($conn, $sql_select);
             $row = $res->fetch_assoc();
@@ -82,8 +74,6 @@ function insertCategory($conn, $product, $product_id)
                 $sql = "INSERT INTO a_relation_product_category (product_id, category_id) VALUES ('{$product_id}', '{$category_id}')";
                 insertToDB($conn, $sql);
             } else {
-                echo "\n" . "Нет такого раздела" . "\n";
-
                 $sql = "INSERT INTO a_category (name) VALUES ('{$value}')";
                 insertToDB($conn, $sql);
 
@@ -99,7 +89,7 @@ function insertCategory($conn, $product, $product_id)
     }
 }
 
-function parseXml($xml)
+function parseXml($xml) // Парсит xml и раскладывает по таблицам БД
 {
     $conn = dataBaseConnect();
 
@@ -108,18 +98,11 @@ function parseXml($xml)
         $code = $product["Код"];
         $name = $product["Название"];
 
-        echo $code . "\n";
-        echo $name . "\n";
-
         $sql = "INSERT INTO a_product (code, name) VALUES ('{$code}', '{$name}')";
         if (mysqli_query($conn, $sql)) {
             echo "\n" ."New record created successfully";
-
             $product_id = getProductId($conn, $code, $name);
-            echo "\n" . "product_id = " .$product_id . "\n";
-
             insertPrice($conn, $product, $product_id);
-
             insertProperty($conn, $product, $product_id);
             insertCategory($conn, $product, $product_id);
 
@@ -130,7 +113,7 @@ function parseXml($xml)
 
     }
 }
-function importXml($a)
+function importXml($a) // Финальная функция, если фаил доступет, то парсит и кладет в БД
 {
     if (file_exists($a)) {
         $xml = simplexml_load_file($a);
